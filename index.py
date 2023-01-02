a6c216aa57c32e874c77f89d4c82198a2dd281ab
jdel17 <jessica.delourme@sfr.fr>
2023-01-02 11:58:16 +0100

    essai commande suivante

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ from flask import Flask,render_template,request,redirect,url_for
 
 app=Flask(__name__)
 liste=[]
-valider=[]
+premiereCommande=[]
+commandesuivante=1
+commandeSuivante=[]
 
 @app.route('/')
 def page_index():
@@ -13,13 +15,31 @@ def page_saisiecommande():
     if request.method=='POST':
         global liste
         data = request.form.to_dict()
+        liste.append(data)
+        premierecommande=liste[0]
+        premiereCommande.append(premierecommande)
+        for commandesuivante in liste:
+            commandesuivante=len(liste)-1
+            commandesuivante=liste[commandesuivante]
+            commandeSuivante.append(commandesuivante)
+            print(commandesuivante)
         if 'enregistrer' in data:
-            liste.append(data)
-            premierecommande=liste[0]
-            print(premierecommande)
             return redirect(url_for('page_livraison'))
     return render_template('saisiecommande.html')
 
-@app.route('/livraison')
+@app.route('/livraison',methods=['GET','POST'])
 def page_livraison():
-    return render_template('livraison.html',commande=liste)
+    if request.method == 'POST':
+        global liste
+        data = request.form.to_dict()
+        if 'button_terminer' in data:
+            premierecommande = liste[0]
+            premierecommande = premierecommande + 1
+            premiereCommande.append(premierecommande)
+            commandesuivante = commandesuivante - premierecommande
+            for commandesuivante in liste:
+                commandesuivante = len(liste) - 1
+                commandesuivante = liste[commandesuivante]
+                commandeSuivante.append(commandesuivante)
+                print(commandesuivante)
+    return render_template('livraison.html',premierecommande=premiereCommande,commandesuivante=commandeSuivante)
