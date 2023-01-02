d247524059cb56e49809471540fd09abb680af9c
jdel17 <jessica.delourme@sfr.fr>
2023-01-02 09:59:08 +0100

    recuperation des données saisies dans le formulaire

--- index.py
+++ index.py
@@ -1,15 +1,23 @@
 from flask import Flask,render_template,request,redirect,url_for
 
 app=Flask(__name__)
+liste=[]
 
 @app.route('/')
 def page_index():
     return render_template('index.html')
 
-@app.route('/saisiecommande')
+@app.route('/saisiecommande',methods=['GET','POST'])
 def page_saisiecommande():
+    if request.method=='POST':
+        global liste
+        data = request.form.to_dict()
+        if 'enregistrer' in data:
+            liste.append(data)
+            print(data)
+            return redirect(url_for('page_livraison'))
     return render_template('saisiecommande.html')
 
 @app.route('/livraison')
 def page_livraison():
-    return render_template('livraison.html')
+    return render_template('livraison.html',commande=liste)
