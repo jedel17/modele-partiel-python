c29b27c08435154b8f75cc78673f70b2aff980f2
jdel17 <jessica.delourme@sfr.fr>
2023-01-02 09:19:23 +0100

    creation des feuilles et création de la page de formulaire

--- index.py
+++ index.py
@@ -5,3 +5,11 @@ app=Flask(__name__)
 @app.route('/')
 def page_index():
     return render_template('index.html')
+
+@app.route('/saisiecommande')
+def page_saisiecommande():
+    return render_template('saisiecommande.html')
+
+@app.route('/livraison')
+def page_livraison():
+    return render_template('livraison.html')
